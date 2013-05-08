463e7119b7fc2ee8dd32a9d0e4cb9b7736dcea99
Sean Sweeney <ssweeney@cambridgema.gov>
2013-05-08 14:19:50 -0400

    Added agsstatusfromlist.py

---
+++ agsstatusfromlist.py
@@ -0,0 +1,66 @@
+#! python
+
+# url fetch
+import urllib
+
+# System libraries
+import sys
+
+# Command line args
+import argparse 
+
+# Common arcgis server functions
+from agsextras import getArgs, getToken, readList, RequestException, JsonErrorException, sendRequest
+
+def main(argv=None):
+    # Create the parser object here and pass it in so script-specific arguments can be added if necessary
+    parser = argparse.ArgumentParser()
+    # Add script-specific arguments before passing in the parser
+    args = getArgs(parser)
+
+    # These may be configuration dependant.
+    # Can add args above if necessary
+    serverPort = 6080
+    folder = 'root'
+
+    startedList = readList(args.filename)
+
+    # Get a token
+    token = getToken(args.user, args.password, args.server, serverPort)
+    if token == "":
+        print "Could not generate a token with the username and password provided."
+        return
+    
+    # Construct URL to read folder - handles folders other than root for future enhancement
+    if str.upper(folder) == "ROOT":
+        folder = ""
+    else:
+        folder += "/"
+            
+    # The requests used below only need the token and the response formatting parameter (json)
+    params = urllib.urlencode({'token': token, 'f': 'json'})
+    # The request headers are also fixed
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    
+    # Loop through each service in the list and get the status
+    for fullSvcName in startedList:
+        # Construct URL to get the status, then make the request                
+        reqURL = "/arcgis/admin/services/" + folder + fullSvcName + "/status"
+        # Post the request
+        try:
+            data = sendRequest(args.server, serverPort, reqURL, params, headers)
+        except RequestException:
+            print "Error while checking status for " + fullSvcName
+            return
+        except JsonErrorException as e:
+            print "Error returned when extracting status information for " + fullSvcName + "."
+            print str(e)
+            return
+
+        print fullSvcName + ": " + data['realTimeState']
+
+    return
+
+# Script start
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
